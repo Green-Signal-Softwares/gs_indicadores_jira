5cd3a705dc39dcad630983d4db88c11963d7d853
davimp3 <davi.nepomuceno@icloud.com>
2025-08-28 20:03:50 -0300

    Versão 1.1

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def initialize_ss_data():
     if 'metricas_agosto' not in st.session_state:
-        df = pd.read_csv ("Dados /Jira_Agosto - Data_Base.csv")
+        df = pd.read_csv ("Jira_Agosto - Data_Base.csv")
         st.session_state['metricas_agosto'] = df
         
